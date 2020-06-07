3f57b1ed10d36386c7c5190fa46584817e42d510
Ryan Gilbert <ryan.gilbert@formidable.com>
2020-06-06 21:58:27 -0600

    implement todo query mutation & subscription

--- src/App.js
+++ src/App.js
@@ -2,25 +2,85 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import gql from 'graphql-tag';
+import { graphql, compose } from 'react-apollo';
+import { graphqlMutation } from 'aws-appsync-react';
+import { buildSubscription } from 'aws-appsync';
+
+const SubscribeToTodos = gql`
+  subscription {
+    onCreateTodo {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const CreateTodo = gql`
+  mutation CreateTodo($input: CreateTodoInput!) {
+    createTodo(input: $input) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const ListTodos = gql`
+  query {
+    listTodos {
+      items {
+        id
+        title
+        completed
+      }
+    }
+  }
+`;
+
+function App(props) {
+  const [todo, setTodo] = React.useState('');
+
+  React.useEffect(() => {
+    props.subscribeToMore(buildSubscription(SubscribeToTodos, ListTodos));
+  });
+
+  const addTodo = () => {
+    if (todo === '') {
+      return;
+    }
+    const input = {
+      title: todo,
+      completed: false,
+    };
+    props.createTodo({ input });
+    setTodo('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input onChange={(e) => setTodo(e.target.value)} value={todo} placeholder="Todo name" />
+        <button onClick={addTodo}>Add Todo</button>
+        {props.todos.map((todo) => (
+          <p key={todo.id}>{todo.title}</p>
+        ))}
       </header>
     </div>
   );
 }
 
-export default App;
+export default compose(
+  graphqlMutation(CreateTodo, ListTodos, 'Todo'),
+  graphql(ListTodos, {
+    options: {
+      fetchPolicy: 'cache-and-network',
+    },
+    props: (props) => ({
+      subscribeToMore: props.data.subscribeToMore,
+      todos: props.data.listTodos ? props.data.listTodos.items : [],
+    }),
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Client from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from 'aws-appsync-react';
+import AppSync from './AppSync';
+
+const client = new Client({
+  url: AppSync.aws_appsync_graphqlEndpoint,
+  region: AppSync.aws_appsync_region,
+  auth: {
+    type: 'API_KEY',
+    apiKey: AppSync.aws_appsync_apiKey,
+  },
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WithProvider />
   </React.StrictMode>,
   document.getElementById('root')
 );
